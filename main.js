a451e98685b3da21397e364323e415404c645fea
Serendibcouk <serendib.co.uk@gmail.com>
2024-05-02 20:20:33 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -13,16 +13,31 @@ function validate()
 {
 var email=document.getElementById("email").value;
 var password=document.getElementById("password").value;
+if(email=="pasividanagama@gmail.com"&& password=="Pajanith@123")
+{
+	window.open("https://sites.google.com/view/cooleducations");
+	return false;
+
+}
 if(email=="daham.vida@gmail.com"&& password=="Pajanith@123")
 {
 	window.open("https://sites.google.com/view/cooleducations");
 	return false;
 
 }
+if(email=="sudeshrathnayake@gmail.com"&& password=="Sudesh@123")
+{
+	window.open("https://sites.google.com/view/cooleducations");
+	return false;
+
+}
+
+
 else
 {	
     window.open("alert.html");
 }
+
 
 
 }
