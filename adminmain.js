cffd2dcb9120ff1196dc0cf11183f111018f975c
Serendibcouk <serendib.co.uk@gmail.com>
2024-05-29 17:56:26 +0100

    Add files via upload

--- adminmain.js
+++ adminmain.js
@@ -11,6 +11,10 @@ sign_in_btn.addEventListener("click", () => {
 });
 function validate()
 {
+var blur = document.getElementById('blur');
+blur.classList.toggle('active')
+var popup = document.getElementById('popup');
+popup.classList.toggle('active')
 var email=document.getElementById("email").value;
 var password=document.getElementById("password").value;
 if(email=="pasividanagama@gmail.com"&& password=="Pajanith@123")
@@ -35,9 +39,16 @@ if(email=="sudeshrathnayake@gmail.com"&& password=="Sudesh@123")
 
 else
 {	
-    window.open("adminalert.html");
+    popup.classList.add("open-popup");
 }
 
 
 
 }
+
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+	var popup = document.getElementById('popup');
+    popup.classList.toggle('active')
+  }
